dff9a9e96298938449f5808764fc123a1a8fa349
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-27 17:37:33 +0100

    Update ahorcado.py

--- ahorcado.py
+++ ahorcado.py
@@ -1,6 +1,10 @@
 from tkinter import *
 
+def probar_letra_funcion():
+    print(letraObtenida.get())
+
 raiz = Tk()
+letraObtenida = StringVar()
 raiz.config(width = 1000, height = 600, bg = "blue", relief = "groove", bd = 10)
 juegoFrame = Frame(raiz)
 juegoFrame.config(width = 1000, height = 600, relief = "sunken", bd = 15)
@@ -10,9 +14,9 @@ juegoFrame.pack()
 Label(juegoFrame, text = "Introduce una letra", font = ("Verdana",24)
       ).grid(row = 0, column = 0, padx = 10, pady = 10)
 
-letra = Entry(juegoFrame, width = 2, font = ("Verdana", 24)
+letra = Entry(juegoFrame, width = 2, font = ("Verdana", 24), textvariable = letraObtenida
               ).grid(row = 0, column = 1, padx = 10, pady = 10)
 
-probar_letra = Button(juegoFrame,text="Probar",bg="orange"
+probar_letra = Button(juegoFrame,text="Probar",bg="orange", command = probar_letra_funcion
                       ).grid(row = 1, column = 0, pady = 10)
 raiz.mainloop()
