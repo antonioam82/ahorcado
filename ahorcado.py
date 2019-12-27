438f8071c24b97bb8767ed31b3d78c098eb92d01
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-12-27 19:09:24 +0100

    Update ahorcado.py

--- ahorcado.py
+++ ahorcado.py
@@ -1,12 +1,23 @@
 from tkinter import *
+from random import randint
 
 letrasUsadas = []
 
 def probar_letra_funcion():
     letrasUsadas.append(letraObtenida.get())
+    if letraObtenida.get() in palabra:
+        if palabra.count(letraObtenida.get())>1:
+            for i in range(len(palabra)):
+                if palabra[i]==letraObtenida.get():
+                    guiones[i].config(text=""+letraObtenida.get())
+        else:
+            guiones[palabra.index(letraObtenida.get())].config(text=""+letraObtenida.get())
     #print(letrasUsadas)
 
 raiz = Tk()
+archivo = open("palabras.txt","r")
+conjuntoPalabras = list(archivo.read().split("\n"))
+palabra = conjuntoPalabras[randint(0,len(conjuntoPalabras)-1)].lower()
 letraObtenida = StringVar()
 raiz.config(width = 1000, height = 600, bg = "blue", relief = "groove", bd = 10)
 juegoFrame = Frame(raiz)
@@ -22,4 +33,11 @@ letra = Entry(juegoFrame, width = 2, font = ("Verdana", 24), textvariable = letr
 
 probar_letra = Button(juegoFrame,text="Probar",bg="orange", command = probar_letra_funcion
                       ).grid(row = 1, column = 0, pady = 10)
+
+guiones = [Label(juegoFrame,text="_",font=("verdana",30)) for _ in palabra]
+inicialx=200
+for i in range(len(palabra)):
+    guiones[i].place(x=inicialx,y=400)
+    inicialx+=50
+    
 raiz.mainloop()
